a365d9b14bf957c52206e3282bd616eb81c9b3be
tikap <tikapradhan095@gmail.com>
2021-10-28 18:59:58 -0400

    Set main menu layout and option texts

--- app/src/main/java/com/example/tapmeandsee/MainActivity.kt
+++ app/src/main/java/com/example/tapmeandsee/MainActivity.kt
@@ -3,36 +3,46 @@ package com.example.tapmeandsee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tapmeandsee.ui.theme.TapMeAndSeeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TapMeAndSeeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainMenu()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainMenu() {
+    val padding = 16.dp
+    Column(
+        Modifier
+            .padding(padding)
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Text("Initiate")
+        Text("Set Up")
+        Text("Exit")
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    TapMeAndSeeTheme {
-        Greeting("Android")
-    }
+fun PreviewMainMenu(){
+    MainMenu()
 }
